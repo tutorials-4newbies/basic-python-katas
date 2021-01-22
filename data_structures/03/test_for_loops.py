e2da23bf4fb8204b8f54f60988b2e60a50c91f61
alonisser <alonisser@gmail.com>
2021-01-23 00:57:28 +0200

    Place holder for tuple, and loops

---
+++ data_structures/03/test_for_loops.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class ForLoopsTestCase(unittest.TestCase):
+    def test_something(self):
+        # Load a datastructure like air table
+        # sum data from it
+        
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- datetimes/test_datetime_katas.py
+++ datetimes/test_datetime_katas.py
@@ -2,8 +2,18 @@ import unittest
 
 
 class DatetimeKatasTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_now(self):
+        pass
+
+    def test_datetime_from_datetime_string(self):
+        pass
+
+    def test_datetime_from_arbitary_date(self):
+        pass
+
+    def test_finding_time_delta_seconds(self):
+        pass
 
 
 if __name__ == '__main__':
